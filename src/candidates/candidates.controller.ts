7fb253785526522b14305d6cc67d9476beb7e81c
martin.aguado <martin.aguado@bosonit.com>
2025-07-29 16:25:08 +0200

    feat(candidates): add candidates controller

--- src/candidates/candidates.controller.ts
+++ src/candidates/candidates.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, Body } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CandidatesService } from './candidates.service';
+import { CreateCandidateDto } from './dto/create-candidate.dto';
 
 @Controller('candidates')
-export class CandidatesController {}
+export class CandidatesController {
+    constructor(private readonly svc: CandidatesService) { }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async upload(
+        @UploadedFile() file: Express.Multer.File,
+        @Body('name') name: string,
+        @Body('surname') surname: string
+    ): Promise<CreateCandidateDto[]> {
+        const dtos = await this.svc.parseExcel(file.buffer);
+        return dtos.map(dto => ({ ...dto, name, surname }));
+    }
+}
